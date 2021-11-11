14fe9dbe7672a4a523a9a9837059897a464bab0e
alexvsc1922 <93945053+alexvsc1922@users.noreply.github.com>
2021-11-11 17:24:06 +0000

    Create Estacionamento.cpp
    Co-Authored-By: TiagoShadow65 <94133211+TiagoShadow65@users.noreply.github.com>

---
+++ Estacionamento.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/*
+- permita definir o número de linha e colunas da grelha de lugares do parque;
+*/
+void(int linha, int coluna){
+	int lugares_Estacionamento[linha][coluna];
+}
+
+/*
+- guarde a informação sobre a ocupação do parque, nomeadamente, a indicação, para
+cada viatura de qual o local onde está guardado, a data e hora de entrada, usando uma
+estrutura de dados adequada;
+- permita adicionar/estacionar uma viatura num dos lugares livres;
+- permita retirar uma viatura, ficando o respetivo lugar vazio, e, com base no tempo de
+ocupação, calcular o preço a pagar pelo estacionamento, mediante a tarifa horária
+definida pelo parque (ex: 2.50€/hora);
+- visualize de um forma gráfica simplista a ocupação do parque. Por exemplo:
+[ O L L O]
+[ O O O O]
+[ L L L L]
+*L – Livre
+*O – ocupado 
+*/
+main(){
+	
+}
